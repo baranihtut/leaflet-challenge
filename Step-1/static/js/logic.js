f31c94f4d66b180451ba5e7e2483873162ccd79c
Barani Htut <barani.t16@gmail.com>
2019-11-06 23:16:49 -0800

    Submit

---
+++ Step-1/static/js/logic.js
@@ -0,0 +1,21 @@
+console.log("working");
+
+var map = L.map("mapid", {
+  center: [
+    40.7, -94.5
+  ],
+  zoom: 3
+});
+
+
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+
+    return div;
+  };
+
+  legend.addTo(map);
+});
